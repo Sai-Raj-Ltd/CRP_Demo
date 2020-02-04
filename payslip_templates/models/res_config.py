85542c2b7c0bc51b0b237f813d12bb3b086f2874
Sai-Raj-Ltd <57390445+Sai-Raj-Ltd@users.noreply.github.com>
2020-02-04 16:36:21 +0530

    custom_modules
    new_db

---
+++ payslip_templates/models/res_config.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (https://opensource.org/licenses/LGPL-3.0).
+#
+# This software and associated files (the "Software") may only be used (executed,
+# modified, executed after modifications) if you have purchased a valid license
+# from the authors, typically via Odoo Apps, or if you have received a written
+# agreement from the authors of the Software (see the COPYRIGHT section below).
+#
+# You may develop Odoo modules that use the Software as a library (typically
+# by depending on it, importing it and using its resources), but without copying
+# any source code or material from the Software. You may distribute those
+# modules under the license of your choice, provided that this license is
+# compatible with the terms of the Odoo Proprietary License (For example:
+# LGPL, MIT, or proprietary licenses similar to this one).
+#
+# It is forbidden to publish, distribute, sublicense, or sell copies of the Software
+# or modified copies of the Software.
+#
+# The above copyright notice and this permission notice must be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+#########COPYRIGHT#####
+# © 2016 Bernard K Too<bernard.too@optima.co.ke>
+from odoo import models, fields, api
+
+
+class hr_config_settings(models.TransientModel):
+    _inherit = 'res.config.settings'
+    payslip_logo = fields.Binary(
+        string='Payslip Logo',
+        attachment=True,
+        help="Default Logo for Payslips",
+        related='company_id.payslip_logo', readonly=False)
+    ptemplate_id = fields.Many2one(
+        'ir.ui.view',
+        'Payslip Template',
+        related='company_id.ptemplate_id', readonly=False)
+    detailed_template_id = fields.Many2one(
+        'ir.ui.view',
+        'Detailed Payslip Template',
+        related='company_id.pdetailed_template_id', readonly=False)
+    odd = fields.Char(
+        'Odd parity Color',
+        help="The background color for Odd lines in the payslip",
+        related='company_id.podd',
+        required=True, readonly=False)
+    even = fields.Char(
+        'Even parity Color',
+        help="The background color for Even lines in the payslip",
+        related='company_id.peven',
+        required=True, readonly=False)
+    theme_color = fields.Char(
+        'Theme Color',
+        help="The Main Theme color of the payslip",
+        related='company_id.ptheme_color',
+        required=True, readonly=False)
+    theme_txt_color = fields.Char(
+        'Theme Text Color',
+        help="The font color of the areas with the theme color",
+        required=True, related='company_id.ptheme_txt_color', readonly=False)
+    text_color = fields.Char(
+        'Text Color',
+        help="The Text color of the payslip",
+        related='company_id.ptext_color',
+        required=True, readonly=False)
+    name_color = fields.Char(
+        'Company Name Color',
+        help="The Text color of the Company Name",
+        related='company_id.pname_color',
+        required=True, readonly=False)
+    cust_color = fields.Char(
+        'Employee Name Color',
+        help="The Text color of the Employee Name",
+        related='company_id.pcust_color',
+        required=True, readonly=False)
+    header_font = fields.Selection([(x,
+                                     str(x)) for x in range(1,
+                                                            51)],
+                                   string="Header Font-Size(px):",
+                                   related='company_id.pheader_font',
+                                   required=True, readonly=False)
+    body_font = fields.Selection([(x,
+                                   str(x)) for x in range(1,
+                                                          51)],
+                                 string="Body Font-Size(px):",
+                                 related='company_id.pbody_font',
+                                 required=True, readonly=False)
+    footer_font = fields.Selection([(x,
+                                     str(x)) for x in range(1,
+                                                            51)],
+                                   string="Footer Font-Size(px):",
+                                   related='company_id.pfooter_font',
+                                   required=True, readonly=False)
+    font_family = fields.Char(
+        'Font Family:',
+        related='company_id.pfont_family', readonly=False)
